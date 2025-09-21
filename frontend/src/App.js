87d288849245fcf9e96417cbe1f9c2f72950b4c0
Lesslibls <Lesslialvarez125@gmail.com>
2025-09-21 12:54:33 -0600

    Intalacion de paquetes necesarios para el frontend y configuracion del archivo App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
